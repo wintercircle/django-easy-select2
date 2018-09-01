4c2c7e697b40457a5ab304a8ee27d97762eb76de
Gianni Moschini <gianni.moschini+github@gmail.com>
2018-09-01 11:08:35 +0100

    Remove window.jQuery/window.$ definition for Select2

--- easy_select2/static/easy_select2/js/easy_select2.js
+++ easy_select2/static/easy_select2/js/easy_select2.js
@@ -1,16 +1,5 @@
 var _jq = (jQuery || django.jQuery)
 
-// We need this exports because Select2 needs window.jQuery
-// to initialize. In fact it removes jQuery.noConflict(true) if
-// django.jQuery is used.
-if (window.jQuery == undefined) {
-    window.jQuery = _jq;
-}
-if (window.$ == undefined) {
-    window.$ = _jq;
-}
-
-
 (function ($) {
     "use strict";
     // store for keeping all the select2 widget ids for fail-safe parsing later
